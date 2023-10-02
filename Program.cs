0087e9321036e0e0a87b1e567f77ba1e3d55b704
andruwa_01 <cheprasov.work@gmail.com>
2023-10-02 13:42:48 +0300

    Update github repo

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿    class Program{
-        static void Main(string[] args){
-            Console.WriteLine("Hello world");
+﻿using System;
+
+class Program{
+        static unsafe void Main(string[] args){
+            int number = 0x5dca;
+            Console.WriteLine(number);
         }
     }

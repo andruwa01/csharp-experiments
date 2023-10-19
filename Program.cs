dd3d6e443106e38f397043b186e4801c0f15a567
andruwa_01 <cheprasov.work@gmail.com>
2023-10-19 18:40:08 +0300

    Commit all from 19.10

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Threading.Channels;
-class Program{
-
+﻿class Program{
         static void Main(string[] args){
-            int a = 5;
-            string s = a.ToString();
-            Console.WriteLine(s);
         }
     }
